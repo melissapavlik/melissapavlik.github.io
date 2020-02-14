0e3461ce7f7bfc29fabb6fbda4b07f82a4810095
melissapavlik <m.pavlik@acleddata.com>
2020-02-14 11:07:06 -0500

    edit the formatting of titles

--- scrollytelling/config.js
+++ scrollytelling/config.js
@@ -757,7 +757,7 @@ var config = {
 
 		{
 			id: 'mexico-text',
-			title: '<span style="font-size:50px; line-height:60px; text-align:center; color:black; font-style:bold; font-style:italic; background-color:#5F9ED1;"> As cartels in Mexico fragment and increase their power, violence spreads.</span>',
+			title: '<span style="font-size:50px; line-height:60px; text-align:center; font-style:bold; font-style:italic; color:#5F9ED1;"> As cartels in Mexico fragment and increase their power, violence spreads.</span>',
 			image: '',
 			description: 'A deteriorating security situation, caused in part by weak public institutions and frequent political turnover, has made Mexico one of the most violent countries in the world. 2019 saw a record number of homicides across the country, particularly concentrated in the south.',
 			location: {
@@ -958,7 +958,7 @@ var config = {
 
 		{
 			id: 'africa-intro',
-			title: '<span style="font-size:100px; line-height:100px; color:black; text-align:right; font-style:bold; background-color:#0A98BB;">Africa</span><span style="font-size:40px; line-height:60px; color:white; text-align:right; font-style:italic; background-color:black;"></br>Conflict with the potential to crumble states and wreak regional havoc.</span>',
+			title: '<span style="font-size:100px; line-height:100px; text-align:right; font-style:bold; color:#0A98BB;">Africa</span><span style="font-size:40px; line-height:60px; color:white; text-align:right; font-style:italic; background-color:black;"></br>Conflict with the potential to crumble states and wreak regional havoc.</span>',
 			image: '',
 			description: 'There exist multiple conflicts across Africa which threaten civilians and regional stability. However, three in particular - those in the Sahel, in Somalia, and in Ethiopia -  may see shifts in the coming year that will produce long-lasting and far-reaching consequences. Al Shabaab threatens the fragile Somali government, as across the continent Islamist groups grow more organized and lethal in the Sahel. Meanwhile, new political openness in Ethiopia has led long-simmering ethnic conflicts to devolve into open violence.',
 			location: {
@@ -1092,7 +1092,7 @@ var config = {
 
 		{
 			id: 'sahel-intro',
-			title: '<span style="font-size:100px; line-height:100px; color:white; font-style:bold;">The Sahel</span><span style="font-size:40px; line-height:50px; color:white; font-style:italic;"> Mali, Burkina Faso, Niger, Mauritania and Chad </span><span style="background-color:#C95200; color:black; line-height:50px;"></br> High risk of conflicts diffusing and infecting neighbors</span>',
+			title: '<span style="font-size:100px; line-height:100px; color:#C95200; font-style:bold;">The Sahel</span><span style="font-size:40px; line-height:50px; color:white; font-style:italic;"> Mali, Burkina Faso, Niger, Mauritania and Chad </span><span style="background-color:#C95200; color:black; line-height:50px;"></br> High risk of conflicts diffusing and infecting neighbors</span>',
 			image: '',
 			description:'' ,
 			location: {
@@ -1360,7 +1360,7 @@ var config = {
 
 		{
 			id: 'ethiopia-intro',
-			title: '<span style="font-size:100px; line-height:100px; color:white; font-style:bold;">Ethiopia</span><span style="background-color:#A2C8EC; color:black; line-height:50px;"></br>At risk of increased fragmentation despite a popular and promising leader</span>',
+			title: '<span style="font-size:100px; line-height:150px; color:white; font-style:bold;">Ethiopia</span><span style="background-color:#A2C8EC; color:black; line-height:50px;"></br>At risk of increased fragmentation despite a popular and promising leader</span>',
 			image: '',
 			description: '',
 			location: {
@@ -2433,7 +2433,7 @@ var config = {
 
 		{
 			id: 'iran-intro',
-			title: '<span style="font-size:100px; line-height:100px; color:black; font-style:bold;">Iran</span><span style="background-color:#FFBB7A; color:black; line-height:50px;"></br>High risk of center deteriorating amid regime escalation abroad and at home</span>',
+			title: '<span style="font-size:100px; line-height:150px; background-color:#CFCFCF; color:black; font-style:bold;">Iran</span><span style="background-color:#FFBB7A; color:black; line-height:50px;"></br>High risk of center deteriorating amid regime escalation abroad and at home</span>',
 			image: '',
 			description: '',
 			location: {
@@ -2768,7 +2768,7 @@ var config = {
 
 		{
 			id: 'central-asia-intro',
-			title: '<span style="font-size:100px; line-height:100px; color:#F88D2F; text-align:right; font-style:bold;">Central Asia</span><span style="font-size:40px; line-height:60px; color:white; text-align:right; font-style:italic; background-color:black;"></br>etcetcetcetcetcetc</span>',
+			title: '<span style="font-size:100px; background-color:black; line-height:150px; color:#F88D2F; text-align:right; font-style:bold;">Central Asia</span><span style="font-size:40px; line-height:60px; color:white; text-align:right; font-style:italic; background-color:#CFCFCF; color:black;"></br>etcetcetcetcetcetc</span>',
 			image: '',
 			description: '',
 			location: {
@@ -2902,7 +2902,7 @@ var config = {
 
 		{
 			id: 'afg-intro',
-			title: '<span style="font-size:100px; line-height:150px; background-color:#093746; color:white; font-style:bold;">Afghanistan</span><span style="background-color:black; color:white; line-height:50px;"></br>At risk of rising violence targeting civilians</span>',
+			title: '<span style="font-size:100px; line-height:150px; background-color:#093746; color:white; font-style:bold;">Afghanistan</span><span style="background-color:#CFCFCF; color:black; line-height:50px;"></br>At risk of rising violence targeting civilians</span>',
 			image: '',
 			description: '',
 			location: {
