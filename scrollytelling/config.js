670793869811b6969dc3572fffc66737b2d7329d
melissapavlik <m.pavlik@acleddata.com>
2020-02-13 22:47:08 -0500

    middle east title

--- scrollytelling/config.js
+++ scrollytelling/config.js
@@ -1829,7 +1829,7 @@ var config = {
 
 		{
 			id: 'middle-east-intro',
-			title: 'The Middle East',
+			title: '<span style="font-size:100px; line-height:100px; color:#FF7F0E; text-align:right; font-style:bold;-webkit-text-stroke-width: 1px;-webkit-text-stroke-color: black;">The Middle East</span><span style="font-size:40px;line-height:60px;color:white;text-align:right;font-style:italic;background-color:black;"></br>etcetcetcet cetcetc</span>',
 			image: '',
 			description: '',
 			location: {
