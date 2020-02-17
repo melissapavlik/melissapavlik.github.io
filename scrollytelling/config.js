e2c92bcf7595b8df368668e6b3bdfac2d72039ae
melissapavlik <m.pavlik@acleddata.com>
2020-02-17 17:45:54 -0500

    changed from span to p formatting

--- scrollytelling/config.js
+++ scrollytelling/config.js
@@ -3090,7 +3090,7 @@ var config = {
 
 		{
 			id: 'mexico-intro',
-			title: '<span style="font-size:100px; line-height:150px; color:white; text-align:center; font-style:bold;"> Mexico </span><span style="background-color:#5F9ED1; color:black; line-height:50px; text-align:center;"></br>High risk of cartel criminal market developing into an insurgency</span>',
+			title: '<p style="font-size:100px; line-height:150px; color:white; text-align:center; font-style:bold;"> Mexico </span><span style="background-color:#5F9ED1; color:black; line-height:50px; text-align:center;"></br>High risk of cartel criminal market developing into an insurgency</p>',
 			image: '',
 			description: '',
 			location: {
